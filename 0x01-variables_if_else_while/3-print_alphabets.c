e9ca7f4171f787ff95be67b4ee3baf9b5beddb31
muzzzo <musondachristopher@rocketmail.com>
2023-03-20 09:03:23 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,9 +11,9 @@ int main(void)
         char i;
 
 	for (i = 'a' ; i <= 'z' ; i++)
-                putchar(i);
+		putchar(i);
 	for (i = 'A' ; i <= 'Z' ; i++)
 		putchar(i);
-		putchar('\n');
+	putchar('\n');
 	return (0);
 }
