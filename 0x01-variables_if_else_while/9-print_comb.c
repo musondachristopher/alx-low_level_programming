51e06aa0f05b84ca543ee2d59f57bc0e781ce6af
muzzzo <musondachristopher@rocketmail.com>
2023-03-20 15:31:45 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,16 +5,21 @@
  *
  * Return: always 0
  */
+
 int main(void)
 {
 	int i;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		if (i == 9)
+			putchar(i + '0');
+		else
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	/*putchar('\n');*/
 	return (0);
 }
