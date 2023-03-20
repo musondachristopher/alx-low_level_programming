88bf81d43a6e08a72ee3caa374de49bdc426e9f0
muzzzo <musondachristopher@rocketmail.com>
2023-03-20 06:43:21 -0700

    Task 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
+/* more headers goes there */
+
 /* betty style doc for function main goes there */
 /**
- * main- Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -15,13 +16,6 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative", n);
-	printf("\n");
+
 	return (0);
 }
-
