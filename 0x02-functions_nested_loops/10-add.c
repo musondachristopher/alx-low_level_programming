f4005ac3c6ed429428cbdd4e0bb9c4c17a4c5854
mac <mac@lib-sta07.aui.maincampus.root>
2023-03-23 21:07:25 +0000

    adding

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* add - Adds two values
+* @n1: The first number to be computed
+* @n2: The second number to be coomputed
+* Return: Integer value
+*/
+
+int main(void){
+
+int n1;
+int n2;
+
+int add(int n1,int n2)
+
+{
+
+return (n1 +n2);
+
+}
