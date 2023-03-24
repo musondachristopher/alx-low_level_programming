28a09c8fe72e2d9f40c92453ace14ea912ea2f97
Muzzzo <musondachristopher@rocketmail.com>
2023-03-24 23:24:32 +0000

    Sum of numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,20 +2,12 @@
 
 /**
 * add - Adds two values
-* @n1: The first number to be computed
-* @n2: The second number to be coomputed
-* Return: Integer value
+* @a: The first number to be computed
+* @b: The second number to be computed
+* Return: add
 */
 
-int main(void){
-
-int n1;
-int n2;
-
-int add(int n1,int n2)
-
+int add(int a, int b);
 {
-
-return (n1 +n2);
-
+	return (a + b);
 }
