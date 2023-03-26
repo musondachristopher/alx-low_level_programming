0424d9914617067352a9ef7b928409cfe23a171a
Muzzzo <musondachristopher@rocketmail.com>
2023-03-26 22:17:28 +0000

    3-print_remaining_days.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -19,15 +19,17 @@ int largest_number(int a, int b, int c)
 	else if (a > c && c > b)
 		largest = a;
 	else if (b > a && a > c)
+	{
 		largest = b;
 	{
 	else if (b > c && c > a)
 		largest = b;
 	else if (c > a &&  a > b)
-		larget = c;
+		largest = c;
 	else
 	{
 		largest = c;
 	}
+
 	return (largest);
 }
