2c93febd26994910942e28fc75e03e44e33c22b2
Muzzzo <musondachristopher@rocketmail.com>
2023-03-28 13:21:30 +0000

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print the numbers from 0 to 90
+ * Return: range of numbers from 0 to 90
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 0; x++)
+	{
+	_putchar(x + '0');
+	}
+	_putchar('\n');
+}
