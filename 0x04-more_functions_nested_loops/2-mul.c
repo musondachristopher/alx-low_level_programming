d040b9aec27b8838045c709145a60a6aff860d07
Muzzzo <musondachristopher@rocketmail.com>
2023-03-28 13:15:20 +0000

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multipicaxn of two numbers
+ * @x: first number
+ * @y: second number
+ * Return: reults of multiple of x and y
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
