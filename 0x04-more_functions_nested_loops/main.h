da6783616eeb5b43518567313d4e94ec9abc016e
Muzzzo <musondachristopher@rocketmail.com>
2023-03-29 15:43:07 +0000

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 int _isupper(int c);
 
 int _isdigit(int c);
@@ -5,6 +10,8 @@ int _isdigit(int c);
 int mul(int a, int b);
 
 void print_numbers(void);
+
+void _putchar(char c);
 
 void print_most_numbers(void);
 
@@ -17,3 +24,5 @@ void print_diagonal(int n);
 void print_square(int size);
 
 void print_triangle(int size);
+
+#endif /* MAIN_H */
