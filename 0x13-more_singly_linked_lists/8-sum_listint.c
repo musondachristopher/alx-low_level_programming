06b77db0d113d82d7d49a1a0832e1a9ceb45d807
Muzzzo <musondachristopher@rocketmail.com>
2023-04-29 17:11:23 +0000

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -7,16 +7,17 @@
  *
  * Return: sum of all the data (n).
  */
+
 int sum_listint(listint_t *head)
 {
-    int sum;
+	int sum;
 
-    sum = 0;
-    while (head != NULL)
-    {
-        sum += head->n;
-        head = head->next;
-    }
+	sum = 0;
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
 
-    return (sum);
+	return (sum);
 }
