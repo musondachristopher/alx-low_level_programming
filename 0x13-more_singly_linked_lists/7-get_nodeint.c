4fc5b5aa5a8c49b97e4b4aebd0cf94e039c4d6d3
Muzzzo <musondachristopher@rocketmail.com>
2023-04-29 17:05:50 +0000

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,14 +7,15 @@
  *
  * Return: nth node. If node does not exist, returns NULL.
  */
+
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < index && head != NULL; i++)
-    {
-        head = head->next;
-    }
+	for (i = 0; i < index && head != NULL; i++)
+	{
+		head = head->next;
+	}
 
-    return (head);
+	return (head);
 }
