457bdf19531ef443ad44485eca7cfd07cd479c7f
muzzzo <musondachristopher@rocketmail.com>
2023-08-23 10:56:21 -0700

    twabwekeshapo

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # by - muzzo
+# A C Script that creates a dynamic library called liball.so from all the .c files that are in the current directory. 
 gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
 gcc -c -fPIC *.c
 gcc -shared -o liball.so *.o
