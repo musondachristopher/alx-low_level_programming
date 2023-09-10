8e9a138afed5f12b5c3151b4a18a48ce53dd914d
muzzzo <musondachristopher@rocketmail.com>
2023-09-10 04:57:06 -0700

    yabwela-bwela

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -17,10 +17,11 @@
  */
 typedef struct hash_node_s
 {
-    char *key;
-    char *value;
-    struct hash_node_s *next;
-} hash_node_t;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+}
+hash_node_t;
 
 /**
  * struct hash_table_s - Hash table data structure
@@ -31,17 +32,16 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-    unsigned long int size;
-    hash_node_t **array;
-} hash_table_t;
+	unsigned long int size;
+	hash_node_t **array;
+}
+hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
-unsigned long int key_index(const unsigned char *key,
-        unsigned long int size);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 hash_node_t *set_pair(const char *key, const char *value);
-int set_pair_only(hash_table_t *ht, const char *key,
-        const char *value, unsigned long int size);
+int set_pair_only(hash_table_t *ht, const char *key, const char *value, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
@@ -63,12 +63,13 @@ void hash_table_delete(hash_table_t *ht);
  */
 typedef struct shash_node_s
 {
-    char *key;
-    char *value;
-    struct shash_node_s *next;
-    struct shash_node_s *sprev;
-    struct shash_node_s *snext;
-} shash_node_t;
+	char *key;
+	char *value;
+	struct shash_node_s *next;
+	struct shash_node_s *sprev;
+	struct shash_node_s *snext;
+}
+shash_node_t;
 /**
  * struct shash_table_s - sorted hash tsble data struct
  *
@@ -82,19 +83,18 @@ typedef struct shash_node_s
  */
 typedef struct shash_table_s
 {
-    unsigned long int size;
-    shash_node_t **array;
-    shash_node_t *shead;
-    shash_node_t *stail;
-} shash_table_t;
+	unsigned long int size;
+	shash_node_t **array;
+	shash_node_t *shead;
+	shash_node_t *stail;
+}
+shash_table_t;
 
 shash_table_t *shash_table_create(unsigned long int size);
 shash_node_t *set_spair(const char *key, const char *value);
-shash_node_t *set_spair_only(shash_table_t *ht, const char *key,
-        const char *value, unsigned long int index);
+shash_node_t *set_spair_only(shash_table_t *ht, const char *key, const char *value, unsigned long int index);
 int update_value(shash_node_t *node, const char *value);
-shash_node_t *set_spair_front(shash_table_t *ht, const char *key,
-        const char *value, unsigned long int index);
+shash_node_t *set_spair_front(shash_table_t *ht, const char *key, const char *value, unsigned long int index);
 int slist_set_first(shash_table_t *ht, shash_node_t *node);
 int slist_set(shash_table_t *ht, shash_node_t *old_node, shash_node_t *node);
 int slist_set_end(shash_table_t *ht, shash_node_t *node);
